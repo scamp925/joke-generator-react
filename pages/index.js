0f072371165419f14d374adcfe7f0c12997b7050
Sariah Campopiano <scampopiano925@gmail.com>
2022-07-11 23:01:09 -0500

    Wrote if...else in handleClick to change the btnText depending on previous btnText. Joke setup and delivery also render depending on the state of the btnText

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import getJoke from '../api/jokeData';
+
 function Home() {
+  const [joke, setJoke] = useState({});
+  const [btnText, setBtnText] = useState('Get a Joke');
+
+  const handleClick = () => {
+    if (btnText === 'Get a Joke' || btnText === 'Get Another Joke') {
+      getJoke().then(setJoke).then(() => {
+        setBtnText('Get Punchline'); // Had to put this in a .then b/c the previous joke setup would stay on the DOM until the API promise came in with the new joke's setup.
+      });
+    } else if (btnText === 'Get Punchline') {
+      setBtnText('Get Another Joke');
+    }
+  };
+
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
@@ -9,7 +26,9 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Welcome Home!</h1>
+      <h2>{ btnText === 'Get a Joke' ? '' : joke.setup}</h2>
+      <h4>{ btnText === 'Get Another Joke' ? joke.delivery : ''}</h4>
+      <Button variant="light" size="lg" onClick={handleClick}>{btnText}</Button>{' '}
     </div>
   );
 }
